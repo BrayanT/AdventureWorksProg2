7e1ea858d9d852a74fd2c44181714978ebae6119
BrayanT <brayan.torres7714@gmail.com>
2025-06-08 23:45:54 -0600

    Constante

--- Models/Constans.cs
+++ Models/Constans.cs
@@ -7,6 +7,7 @@ namespace AdventureWorks_POC.Models
 {
 	public static class Constans
 	{
-        public static string cadena = "Data Source=(local);Initial Catalog=AdventureWorks;Integrated Security=true";
+        public static string cadena = "Server=tcp:adventureworks293084.database.windows.net,1433;Initial Catalog=adventureworks293084;Persist Security Info=False;User ID=adventureworks293084;Password=Cambiodeclave01;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        //public static string cadena = "Data Source=(local);Initial Catalog=AdventureWorks;Integrated Security=true";
     }
 }
